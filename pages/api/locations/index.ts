e9c229de968cea56fc12b995749db9d6292deb66
abby-adroit <abby.adroit@gmail.com>
2022-10-25 17:58:18 +0800

    promise

--- pages/api/locations/index.ts
+++ pages/api/locations/index.ts
@@ -9,22 +9,26 @@ export default authenticated(async function getLocation(
   res: NextApiResponse
 ) {
 
+    
     if(req.method === 'GET'){
         mssql.connect(config, (err: any) => {
             if (err) console.log(err);
         });
-
-        mssql.connect(config).then( () => {
-            return mssql.query `SELECT IncomingGroupLocationID as grpLocId, IncomingGroupID as grpId, GroupName as grpName, LocationID as locationId, LocationName as locationName, StatusID as statusId, StationName as stationName FROM location ORDER BY GroupName ASC`;
-        }).then( (result: { recordsets: any[]; }) => {
-            res.json(result.recordsets[0]);
-            console.log("location loaded");
-        }).catch( (err: any)  => {
-            console.error(err);
-            res.status(500).json(-2);
-        })
+        return new Promise<void>( (resolve) => {
+            mssql.connect(config).then( () => {
+                return mssql.query `SELECT IncomingGroupLocationID as grpLocId, IncomingGroupID as grpId, GroupName as grpName, LocationID as locationId, LocationName as locationName, StatusID as statusId, StationName as stationName FROM location ORDER BY GroupName ASC`;
+            }).then( (result: { recordsets: any[]; }) => {
+                res.json(result.recordsets[0]);
+                // console.log("location loaded");
+                resolve();
+            }).catch( (err: any)  => {
+                console.error(err);
+                res.status(500).send(-2);
+                resolve();
+            })
+        });
     }else{
         console.log("method should be GET")
-        res.status(405).json(-1);
+        res.status(405).send(-1);
     }
 });
